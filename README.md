313dcdd8e7b7fb2c777b62896aa2f2622d3faa07
Bren T <paxtbiz@gmail.com>
2023-02-17 18:28:09 -0800

    updated user data and readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# Social Netowork API Applcation
+
+## Description
+The objective of this project is to develop an API for a social network web application. This platform allows users to express their opinions, respond to the thoughts of their friends, and build a network of connections. The implementation is based on Express.js for handling routing, MongoDB for data storage, and Mongoose ODM for data modeling.
+
+[Video Demo](https://app.castify.com/view/9feb45fc-ef16-42f3-931e-3fe4053029e0)
+
+## Table of Contents
+1.  [Title](#title)
+2.  [Description](#description)
+3.  [Installation](#installation)
+4.  [Usage](#usage)
+5.  [License](#license)
+6.  [Contributing](#contributing)
+7.  [Questions](#questions)
+
+## Installation
+To installa and run this application follow these steps. Download the code and open it up in your editor tool. Open the terminal and `npm i`. You will need to have node installed on your machine. Once all dependencies are downloaded run `npm run seed`. This will run all the applications seeded data. Finally to run the application type `npm start`, this will start the application.
+
+## Usage
+To use this application, or run it in your local host open up your internet browser. In the URL bar type `localhost:3001`. This is the base of the application. From here you can naviate to any of the routes you'd like by using routes after the 3001. If using a tool such as insomnia you may also change the routes to delete/put/get/post to change the data accordingly.
+
+## License
+![MIT](https://img.shields.io/badge/license-MIT-blue)
+
+## Contributing
+Mdn / W3school / U.C. Davis instructor and TA's.
+
+## Questions
+[Brennantaylor98](https://github.com/Brennantaylor98) or contact me at
+  paxtbiz@gmail.com
---
+++ util/data.js
@@ -0,0 +1,73 @@
+const userData = [
+  { username: 'Aaran', email: 'Aaran@gmail.com' },
+  { username: 'Mike', email: 'Mike@gmail.com' },
+  { username: 'Luke', email: 'Luke@gmail.com' },
+  { username: 'Ensar', email: 'Ensar@gmail.com' },
+  { username: 'Fenrir', email: 'Fenrir@gmail.com' },
+  { username: 'Doretta', email: 'Doretta@gmail.com' },
+  { username: 'Zoroaster', email: 'Zoroaster@gmail.com' },
+  { username: 'Edvaldo ', email: 'Edvaldo@gmail.com' },
+  { username: 'Shura', email: 'Shura@gmail.com' },
+];
+
+const thoughtData = [
+  {
+    thoughtText:
+      'Oh he decisively impression attachment friendship so if everything. Whose her enjoy chief new young. Felicity if ye required likewise so doubtful. On so attention necessary at by provision otherwise existence direction.',
+    username: 'Aaran',
+  },
+  {
+    thoughtText:
+      'Up maids me an ample stood given. Certainty say suffering his him collected intention promotion. Hill sold ham men made lose case. Views abode law heard jokes too. Was are delightful solicitude discovered collecting man day.',
+    username: 'Mike',
+  },
+  {
+    thoughtText:
+      'Tiled say decay spoil now walls meant house. My mr interest thoughts screened of outweigh removing. Evening society musical besides inhabit ye my. Lose hill well up will he over on.',
+    username: 'Luke',
+  },
+  {
+    thoughtText:
+      'One advanced diverted domestic sex repeated bringing you old. Possible procured her trifling laughter thoughts property she met way. Companions shy had solicitude favourable own. Which could saw guest man now heard but. Lasted my coming uneasy marked so should.',
+    username: 'Ensar',
+  },
+  {
+    thoughtText:
+      'Procuring education on consulted assurance in do. Is sympathize he expression mr no travelling. Preference he he at travelling in resolution. So striking at of to welcomed resolved. Northward by described up household therefore attention.',
+    username: 'Fenrir',
+  },
+  {
+    thoughtText:
+      'Still court no small think death so an wrote. Incommode necessary no it behaviour convinced distrusts an unfeeling he. Could death since do we hoped is in. Exquisite no my attention extensive. The determine conveying moonlight age.',
+    username: 'Doretta',
+  },
+  {
+    thoughtText:
+      'Among going manor who did. Do ye is celebrated it sympathize considered. May ecstatic did surprise elegance the ignorant age. Own her miss cold last. It so numerous if he outlived disposal. How but sons mrs lady when.',
+    username: 'Zoroaster',
+  },
+  {
+    thoughtText:
+      'Over fact all son tell this any his. No insisted confined of weddings to returned to debating rendered. Keeps order fully so do party means young. Table nay him jokes quick. In felicity up to graceful mistaken horrible consider. Abode never think to at.',
+    username: 'Edvaldo ',
+  },
+  {
+    thoughtText:
+      'Instrument cultivated alteration any favourable expression law far nor. Both new like tore but year. An from mean on with when sing pain. Oh to as principles devonshire companions unsatiable an delightful. The ourselves suffering the sincerity.',
+    username: 'Shura',
+  },
+];
+
+const reactionData = [
+  { username: 'Mike', reactionBody: 'amazing' },
+  { username: 'Aaran', reactionBody: 'wow' },
+  { username: 'Ensar', reactionBody: 'cool' },
+  { username: 'Luke', reactionBody: 'I see' },
+  { username: 'Doretta', reactionBody: 'Love it' },
+  { username: 'Fenrir', reactionBody: 'great' },
+  { username: 'Edvaldo ', reactionBody: 'werid' },
+  { username: 'Shura', reactionBody: 'hate it' },
+  { username: 'Zoroaster', reactionBody: 'uh' },
+];
+
+module.exports = { userData, thoughtData, reactionData };
